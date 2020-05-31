47ffa7db53d330d55ed6f0bad4a3103117c9e9ed
Daniel Carrasco <dac2229@gmail.com>
2020-05-30 19:35:23 -0700

    files uses in How to Write Go Code

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
